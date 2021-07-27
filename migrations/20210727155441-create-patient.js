e5e092695a74b617118b376ac5dfa266cd4f2b02
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-07-27 11:49:49 -0500

    added sequelize

---
+++ migrations/20210727155441-create-patient.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Patients', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      birthdate: {
+        type: Sequelize.DATEONLY
+      },
+      covid: {
+        type: Sequelize.BOOLEAN
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Patients');
+  }
+};
